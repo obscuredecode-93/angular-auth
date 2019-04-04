ec73168464456a2331f12de2610e4de1f6536ce8
Anurag Iyer <anurag.l.iyer@gmail.com>
2019-04-04 17:05:26 -0500

    first commit

--- src/app/helpers/fake-backend.ts
+++ src/app/helpers/fake-backend.ts
@@ -5,7 +5,7 @@ export function fakeBackendFactory(
     backend: MockBackend, 
     options: BaseRequestOptions) {
         
-  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik1vc2ggSGFtZWRhbmkiLCJhZG1pbiI6dHJ1ZX0.iy8az1ZDe-_hS8GLDKsQKgPHvWpHl0zkQBqy1QIPOkA';
+  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik1vc2ggSGFtZWRhbmkiLCJhZG1pbiI6dHJ1ZX0.1dm4jAzSnmfPFNKXAz36Iq6I1upjQ3jW1kTfv5cx2XA';
     
   backend.connections.subscribe((connection: MockConnection) => {
     // We are using the setTimeout() function to simulate an 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-
+import 'rxjs/add/operator/map';
+import { JwtHelper, tokenNotExpired } from 'angular2-jwt';
 @Injectable()
 export class AuthService {
   constructor(private http: Http) {
@@ -8,14 +9,30 @@ export class AuthService {
 
   login(credentials) { 
    return this.http.post('/api/authenticate', 
-      JSON.stringify(credentials));
+      JSON.stringify(credentials))
+      .map(response => {
+      let result = response.json();
+      if(result && result.token){
+        localStorage.setItem('token',result.token);
+        return true;
+      }
+      return false;
+      });
   }
 
   logout() { 
+    localStorage.removeItem('token');
   }
 
   isLoggedIn() { 
-    return false;
+    return tokenNotExpired();
+  }
+
+  get currentUser(){
+    let token = localStorage.getItem('token');
+    if(!token) return null;
+    return new JwtHelper().decodeToken(token);
+
   }
 }
 
